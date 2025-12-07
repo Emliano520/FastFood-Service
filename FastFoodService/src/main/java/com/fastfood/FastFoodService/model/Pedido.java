37d075b592855d1d546bd75ef0f6e4f9536f94e1
Emliano520 <20243ds133@utez.edu.mx>
2025-12-07 00:31:16 -0600

    Se implemento la clase Pedido con sus atributos correspondientes, asi como constructores y metodo toString

---
+++ FastFoodService/src/main/java/com/fastfood/FastFoodService/model/Pedido.java
@@ -0,0 +1,73 @@
+package com.fastfood.FastFoodService.model;
+
+public class Pedido {
+
+    private int id;
+    private String nombreCliente;
+    private String descripcion;
+    private double monto;
+    private String estado;
+
+    public Pedido(){
+    }
+
+    public Pedido(int id, String nombreCliente, String descripcion, double monto, String estado) {
+
+        this.id = id;
+        this.nombreCliente = nombreCliente;
+        this.descripcion = descripcion;
+        this.monto = monto;
+        this.estado = estado;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    public void setNombreCliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Pedido{" +
+                "id=" + id +
+                ", nombreCliente='" + nombreCliente + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", monto=" + monto +
+                ", estado='" + estado + '\'' +
+                '}';
+    }
+}
